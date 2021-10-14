2fb0f981a7c32e0867fc3c1aa803b3344278bce7
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 11:50:03 -0600

    Create 1.16.1.cpp

---
+++ Chapter 1 - Introduction to C++/1.16.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   // Draw tree
+   cout << "   *"    << endl;
+   cout << "  ***"   << endl;
+   cout << " *****"   << endl;
+   cout << "*******"   << endl;
+   cout << "  ***" << endl;
+   cout << endl;
+   cout << endl;
+   cout << "/\\   /\\"    << endl;
+   cout << "  o o"   << endl;
+   cout << " =   ="   << endl;
+   cout << "  ---"   << endl;
+   
+
+   return 0;
+}
