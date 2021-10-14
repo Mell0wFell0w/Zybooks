21abff8269d886e44d4e9c98f409002bfd4829c9
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 11:58:13 -0600

    Create 2.2.2.cpp

---
+++ Chapter 2 - Variables and Assignments/2.2.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int numCoins;
+   int numNickels;
+   int numDimes;
+
+   numNickels = 5;
+   numDimes = 6;
+
+   /* Your solution goes here  */
+    numCoins = numNickels + numDimes;
+
+   cout << "There are " << numCoins << " coins" << endl;
+
+   return 0;
+}
