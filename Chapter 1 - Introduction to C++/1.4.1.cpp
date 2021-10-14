ef3cd4a2641426d4a247b7e57069470a6c134295
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-13 18:26:30 -0600

    Create 1.4.1.cpp

---
+++ Chapter 1 - Introduction to C++/1.4.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  /* Your solution goes here  */
+   cout << "Hello world!" << endl;
+   cout << "How are you?" << endl;
+   cout << "   (I'm fine)." << endl;
+   
+
+   return 0;
+}
