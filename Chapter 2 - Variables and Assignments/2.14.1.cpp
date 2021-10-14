e927c985f3b7223e9ec202d895e969104758afef
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 12:15:28 -0600

    Create 2.14.1.cpp

---
+++ Chapter 2 - Variables and Assignments/2.14.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char letterToQuit;
+   int  numPresses;
+
+   cin >> letterToQuit;
+   cin >> numPresses;
+
+   /* Your solution goes here  */
+   cout << "Press the " << letterToQuit << " key " << numPresses << " times to quit." << endl;
+
+   return 0;
+}
