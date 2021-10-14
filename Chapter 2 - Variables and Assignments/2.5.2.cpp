36991f67e7312fbd3ed0cfb49091b15ac19a3433
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 12:02:32 -0600

    Create 2.5.2.cpp

---
+++ Chapter 2 - Variables and Assignments/2.5.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int num1;
+   int num2;
+   int finalResult;
+
+   cin >> num1;
+   cin >> num2;
+
+   /* Your solution goes here  */
+   finalResult = (num1 + num2) / 3;
+
+   cout << "Final result: " << finalResult << endl;
+
+   return 0;
+}
