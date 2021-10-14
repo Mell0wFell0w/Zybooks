508595cdb9ced3de127d26cf38b349036f504c03
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 12:01:12 -0600

    Create 2.2.4.cpp

---
+++ Chapter 2 - Variables and Assignments/2.2.4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int numPeople;
+
+   cin >> numPeople;
+
+   /* Your solution goes here  */
+   numPeople = numPeople + 5;
+
+   cout << "There are " << numPeople << " people." << endl;
+
+   return 0;
+}
