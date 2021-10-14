157fb9783b5a43fb69ef9808124268500b48366e
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 11:50:36 -0600

    Create 1.17.1.cpp

---
+++ Chapter 1 - Introduction to C++/1.17.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "\t\t\t\tSimple BYU Trivia" << endl;
+   cout << endl;
+   cout << "\t\tQuestions\t\t\t\tAnswers" << endl;
+   cout << endl;
+   cout << "What was the original name of BYU?\t\tBrigham Young Academy" << endl;
+   cout << "When was BYU established?\t\t\t1875" << endl;
+   cout << "Who was the first \"permanent\" principal of BYA?\tKarl G. Maeser" << endl;
+   cout << "When did BYA turn into BYU?\t\t\t1903" << endl;
+   cout << "To what sports conference do we belong?\t\tIndependent \\Football\\" << endl;
+   cout << "When did BYU \"win\" the national football title?\t1984" << endl;
+   cout << "Who won the Heisman Trophy in 1990?\t\tTy Detmer" << endl;
+  
+   
+   return 0;
+}
