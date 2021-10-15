3d7527b4d29314159851d2298f78b0bb8f5d406d
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-14 19:10:43 -0600

    Create 2.15.1.cpp

---
+++ Chapter 2 - Variables and Assignments/2.15.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string firstName;
+   string lastName;
+
+   /* Your solution goes here  */
+   cin >> firstName;
+   cin >> lastName;
+   
+   cout << lastName << ", " << firstName << endl;
+
+   return 0;
+}
+
+/* Write a program that reads a person's first and last names separated by a space, assuming the first and last names are both single words. 
+Then the program outputs last name, first name. End with newline. 
+
+Example output if the input is: Maya Jones 
+Jones, Maya  */
