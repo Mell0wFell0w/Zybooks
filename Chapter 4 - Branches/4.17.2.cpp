7ccdfe987ae614bc1a93228a54cbf5d76b129201
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-27 11:40:44 -0600

    Create 4.17.2.cpp

---
+++ Chapter 4 - Branches/4.17.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+   string passCode;
+
+   cin >> passCode;
+
+   /* Your solution goes here  */
+   if (isalpha(passCode[0])){
+      passCode[0] = '_';
+   }
+   
+   if (isalpha(passCode[1])){
+      passCode[1] = '_'; 
+   }
+
+   cout << passCode << endl;
+   return 0;
+}
+
+/* Replace any alphabetic character with '_' in 2-character string passCode. Ex: If passCode is "9a", output is:
+9_
+Hint: Use two if statements to check each of the two characters in the string, using isalpha(). */
