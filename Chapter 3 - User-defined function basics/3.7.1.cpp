2dc09c7bf67a77c06cb115aacb7eaa76a0a5fd19
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-26 19:54:54 -0600

    Create 3.7.1.cpp

---
+++ Chapter 3 - User-defined function basics/3.7.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/* Your solution goes here  */
+void CoordTransform(int& xVal, int& yVal, int& xValNew, int& yValNew) {
+    xValNew = ((xVal + 1) * 2);
+    yValNew = ((yVal + 1) * 2);
+      }
+/* End of solution */
+
+int main() {
+   int xValNew;
+   int yValNew;
+   int xValUser;
+   int yValUser;
+
+   cin >> xValUser;
+   cin >> yValUser;
+
+   CoordTransform(xValUser, yValUser, xValNew, yValNew);
+   cout << "(" << xValUser << ", " << yValUser << ") becomes (" << xValNew << ", " << yValNew << ")" << endl;
+
+   return 0;
+}
+
+/* Define a function CoordTransform() that transforms the function's first two input parameters xVal and yVal into two output parameters xValNew and yValNew. 
+The function returns void. The transformation is new = (old + 1) * 2. Ex: If xVal = 3 and yVal = 4, then xValNew is 8 and yValNew is 10.  */
