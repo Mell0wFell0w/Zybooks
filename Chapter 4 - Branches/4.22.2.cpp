a64739a9d9cfa13952c38a229cd1152ac70eabfb
Mell0wFell0w <76127568+Mell0wFell0w@users.noreply.github.com>
2021-10-27 11:46:41 -0600

    Create 4.22.2.cpp

---
+++ Chapter 4 - Branches/4.22.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   double outsideTemperature;
+
+   cin >> outsideTemperature;
+
+   /* Your solution goes here  */
+   cout << setprecision(4) << outsideTemperature << endl;
+
+   return 0;
+}
+
+/* Write a single statement that prints outsideTemperature with 4 digits. End with newline. Sample output with input 103.45632:
+103.5 */
